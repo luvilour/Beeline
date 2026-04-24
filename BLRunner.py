d2888b1606614bfb0beb80cc9865ca6019677c27
lvilour <LVilour@student.uliege.be>
2026-04-24 14:16:22 +0200

    Imported the scGeneRAIRunner

--- BLRunner.py
+++ BLRunner.py
@@ -18,6 +18,7 @@ from BLRun.sinceritiesRunner import SINCERITIESRunner
 from BLRun.singeRunner import SINGERunner
 from BLRun.pearsonRunner import PearsonRunner
 from BLRun.LassoNetRunner import LassoNetRunner
+from BLRun.SCGENERAI import scGeneRAIRunner
 
 RUNNERS = {
     'GENIE3':       GENIE3Runner,
